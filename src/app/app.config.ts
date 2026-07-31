663b60a0c5839213804d7bc19ab936f6d3361078
Tiago Assis <tiagocalutis@hotmail.com.br>
2026-07-31 17:27:55 -0300

    feat: adicionar HTTP interceptor para enviar JWT automaticamente

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,17 @@
 import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 import { registerFontAwesomeIcons } from './core/fontawesome';
+import { authInterceptor } from './core/auth/auth.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideAnimationsAsync(),
     provideRouter(routes),
     provideAppInitializer(() => {
